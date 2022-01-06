49568d5600b71e99066179a35035d0a86987b856
prathushapandipati <prathusha.pandipati@gmail.com>
2022-01-06 12:50:37 +0100

    Completed figma to components

---
+++ src/components/Footer.vue
@@ -0,0 +1,30 @@
+<template>
+  <footer>
+    <section class="footer-links">
+      <nav>
+        <h2>Nav</h2>
+        <a href="#" class="link">Products</a>
+        <a href="#" class="link">About us</a>
+        <a href="#" class="link">Contact</a>
+      </nav>
+      <article>
+        <h2>Address</h2>
+        <p>Plinkeplonkvägen 32</p>
+        <p>888 81 Klonkberga</p>
+      </article>
+      <article class="social-media">
+        <h2>Social media</h2>
+        <section class="icons">
+          <img src="./../assets/images/logo-twitter.svg" alt="" />
+          <img src="./../assets/images/logo-facebook.svg" alt="" />
+          <img src="./../assets/images/logo-linkedin.svg" alt="" />
+          <img src="./../assets/images/logo-instagram.svg" alt="" />
+        </section>
+      </article>
+    </section>
+  </footer>
+</template>
+
+<script>
+export default {};
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+  <header>
+    <aside>
+      <img src="./../assets/images/navicon.svg" alt="" />
+    </aside>
+    <h1>Poster shop</h1>
+    <img src="./../assets/images/ninjahead.svg" alt="" />
+  </header>
+</template>
+
+
+
+<script>
+export default {};
+</script>
